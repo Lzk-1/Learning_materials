886f6af461a6eaa9eaad1fd92a1114b377d6534e
lzk-1 <1252195204@qq.com>
2020-10-07 21:20:24 +0800

    天剑程序
    Signed-off-by: lzk-1 <1252195204@qq.com>

---
+++ 01Python编程/01进程线程协程/01代码/03test.py
@@ -0,0 +1,43 @@
+# encoding:utf-8
+import time
+from multiprocessing import Process
+
+
+def computeNum():
+    count = 0
+    i = 0
+    while i<50000000:
+        count += i**2
+        i += 1
+
+def computeNum2():
+    count = 0
+    i = 50000000
+    while i<100000000:
+        count += i**2
+        i += 1
+
+def main():
+  dt1 = time.time()
+  computeNum()
+  computeNum2()
+  dt2 = time.time()
+  dt = dt2 - dt1
+  print(f'正常用时：{dt}')
+
+  time.sleep(5)
+
+  t1 = time.time()
+  p1 = Process(target=computeNum)
+  p2 = Process(target=computeNum2)
+  p1.start()
+  p2.start()
+  p1.join()
+  p2.join()
+  t2 = time.time()
+  print(f'进程用时：{t2-t1}')
+
+
+
+if __name__ == '__main__':
+    main()
