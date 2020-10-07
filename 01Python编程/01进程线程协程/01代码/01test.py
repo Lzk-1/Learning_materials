b5129638c86e8ce61e163e2e4a61e752044b3242
lzk-1 <1252195204@qq.com>
2020-10-07 17:49:54 +0800

    天剑程序
    Signed-off-by: lzk-1 <1252195204@qq.com>

--- 01Python编程/01进程线程协程/01代码/01test.py
+++ 01Python编程/01进程线程协程/01代码/01test.py
@@ -1,5 +1,7 @@
 # encoding:utf-8
+import threading
 import time
+
 
 def computeNum():
     t1 = time.time()
@@ -10,7 +12,7 @@ def computeNum():
         i += 1
     t2 = time.time()
     dt = t2 - t1
-    return dt
+    # return dt
 
 def computeNum2():
     t1 = time.time()
@@ -21,13 +23,19 @@ def computeNum2():
         i += 1
     t2 = time.time()
     dt = t2 - t1
-    return dt
+    # return dt
 
 def main():
-  dt1 = computeNum()
-  dt2 = computeNum2()
-  dt = dt1 + dt2
-  print(dt)
+  # dt1 = computeNum()
+  # dt2 = computeNum2()
+  # dt = dt1 + dt2
+  # print(dt)
+
+  t0 = time.time()
+  t1 = threading.Thread(target=computeNum())
+  t2 = threading.Thread(target=computeNum2())
+  t3 = time.time()
+  print(t3-t0)
 
 
 if __name__ == '__main__':
